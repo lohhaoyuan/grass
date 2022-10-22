c57f1a5cf6edf25acd4597d4a1fda1aec8b3617a
saumilthecode <saumil707@gmail.com>
2022-10-22 10:50:34 +0800

    HAO YUAN SKILL ISSUEDLY DIDNT COMMIT XCODE FILES

---
+++ grass/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  grass
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grass/grassApp.swift
@@ -0,0 +1,17 @@
+//
+//  grassApp.swift
+//  grass
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct grassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
