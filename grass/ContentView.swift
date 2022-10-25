b79b8adf57ac25bcf310dc584795a5dc79a0053d
saumilthecode <saumil707@gmail.com>
2022-10-25 23:54:47 +0800

    green stuff

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -9,24 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-
-            
-            Text("Grass")
-                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-                .font(.monospaced(.largeTitle)())
-                .multilineTextAlignment(.leading)
-            Text("Personalise Your Growing Experience")
-                .font(.body)
-                .multilineTextAlignment(.leading)
-                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-                .lineLimit(nil)
-
-
-        }
-        .padding()
+        
+        
+        foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+            .font(.monospaced(.largeTitle)())
+            .multilineTextAlignment(.leading)
+        Text("Personalise Your Growing Experience")
+            .font(.body)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+        
+        
+        
+        
+        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
