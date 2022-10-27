a25de94e1c3aa6fccb9265d38370ea159640f5c0
saumilthecode <saumil707@gmail.com>
2022-10-27 18:56:23 +0800

    fixed errors

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -9,20 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        
-        foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-            .font(.monospaced(.largeTitle)())
-            .multilineTextAlignment(.leading)
-        Text("Personalise Your Growing Experience")
-            .font(.body)
-            .multilineTextAlignment(.leading)
-            .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-        
-        
-        
-        
-        
+        HStack{
+            Text("Grass")
+                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+                .font(.monospaced(.largeTitle)())
+                .multilineTextAlignment(.leading)
+
+            Text("Personalise Your Growing Experience")
+                .font(.body)
+                .multilineTextAlignment(.leading)
+                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+            
+        }
     }
     
 }
--- grass/grassApp.swift
+++ grass/grassApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @main
+
 struct grassApp: App {
     var body: some Scene {
         WindowGroup {
