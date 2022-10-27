60627310149f36c13859fda71f821dc70d94b6dc
saumilthecode <saumil707@gmail.com>
2022-10-27 19:18:13 +0800

    smexy line

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            Text("Grass")
-                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-                .font(.monospaced(.largeTitle)())
-                .multilineTextAlignment(.leading)
-
-            Text("Personalise Your Growing Experience")
-                .font(.body)
-                .multilineTextAlignment(.leading)
-                .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
-            
+        VStack{
+            HStack{
+                Text("Grass")
+                    .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+                    .font(.monospaced(.largeTitle)())
+                    .multilineTextAlignment(.leading)
+                
+                Divider()
+                    .frame(width: 4.0, height: 75.0)
+                    .overlay(Color(.green))
+                Text("Personalise Your Growing Experience")
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+   
+                
+            }
         }
     }
     
