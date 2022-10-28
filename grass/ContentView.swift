1ffb56f765f68b475054c710e3da8e85d38683aa
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-10-28 20:08:45 +0800

    made button on welcome screen work

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -9,36 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack(){
-                Text("Grass")
-                    .foregroundColor(Color("Swamp Green"))
-                    .font(.monospaced(.largeTitle)())
-                    .multilineTextAlignment(.leading)
-                Divider()
-                    .frame(width: 4.0, height: 75.0)
-                    .overlay(Color("Swamp Green"))
-                Text("Personalise Your Growing Experience")
-                    .font(.body)
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(Color("Swamp Green"))
-   
-                
+        NavigationView{
+            VStack{
+                HStack(){
+                    Text("Grass")
+                        .foregroundColor(Color("Swamp Green"))
+                        .font(.monospaced(.largeTitle)())
+                        .multilineTextAlignment(.leading)
+                    Divider()
+                        .frame(width: 4.0, height: 75.0)
+                        .overlay(Color("Swamp Green"))
+                    Text("Personalise Your Growing Experience")
+                        .font(.body)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color("Swamp Green"))
+                }
+                .padding(70.0)
+                NavigationLink(destination: PlantSelectionView()) {
+                    Text("Get Started")
+                }
+                .padding(.vertical, 20)
+                .padding(.horizontal, 87.75)
+                .foregroundColor(.white)
+                .background(Color(red: -0.123, green: 0.285, blue: 0.225))
+                .cornerRadius(22.0)
             }
-            .padding(70.0)
-            Button("Get Started.") {
-
-            }
-            .navigationTitle("The Entrace Screen")
-            .padding(.vertical, 20)
-            .padding(.horizontal, 87.75)
-            .foregroundColor(.white)
-            .background(Color(red: -0.123, green: 0.285, blue: 0.225))
-            .cornerRadius(22.0)
-
         }
     }
-    
 }
 
 
--- grass/PlantSelectionView.swift
+++ grass/PlantSelectionView.swift
@@ -1,35 +1,32 @@
 //
-//  WhatDoYouWannaGrow.swift
+//  PlantSelectionView.swift
 //  grass
 //
-//  Created by T Krobot on 27/10/22.
+//  Created by James Kuang on 28/10/22.
 //
 
 import SwiftUI
 
-struct WhatDoYouWannaGrow: View {
+struct PlantSelectionView: View {
     var body: some View {
-        
         Text("What do you want to grow?")
             .font(.largeTitle)
             .fontWeight(.semibold)
             .multilineTextAlignment(.leading)
-        
-        
+
         Button("My plant is not here") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            // plant selection thingy
         }
+        
         .padding(.vertical, 20)
         .padding(.horizontal, 87.75)
         .foregroundColor(.white)
         .background(Color(red: -0.123, green: 0.285, blue: 0.225))
-        .cornerRadius(22.0)
-    }
+        .cornerRadius(22.0)    }
 }
 
-
-struct WhatDoYouWannaGrow_Previews: PreviewProvider {
+struct PlantSelectionView_Previews: PreviewProvider {
     static var previews: some View {
-        WhatDoYouWannaGrow()
+        PlantSelectionView()
     }
 }
