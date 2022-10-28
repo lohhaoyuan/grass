f9330524c189e577f09a4b03717b29346ab952ae
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-10-28 19:53:39 +0800

    made colour consistent

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -12,17 +12,16 @@ struct ContentView: View {
         VStack{
             HStack(){
                 Text("Grass")
-                    .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+                    .foregroundColor(Color("Swamp Green"))
                     .font(.monospaced(.largeTitle)())
                     .multilineTextAlignment(.leading)
-                
                 Divider()
                     .frame(width: 4.0, height: 75.0)
-                    .overlay(Color(.green))
+                    .overlay(Color("Swamp Green"))
                 Text("Personalise Your Growing Experience")
                     .font(.body)
                     .multilineTextAlignment(.leading)
-                    .foregroundColor(Color(red: 0.0, green: 0.27843137, blue: 0.22745098, opacity: 100.0))
+                    .foregroundColor(Color("Swamp Green"))
    
                 
             }
