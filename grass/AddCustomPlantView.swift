21eaf7fd33b584d6f0c98bbb4410f1a48f050b8e
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-10-29 09:53:43 +0800

    i forgot

--- grass/AddCustomPlantView.swift
+++ grass/AddCustomPlantView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct AddCustomPlantView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+        }
+        .navigationTitle("Add Custom Plant")
     }
 }
 
