3717cc171594b93ab934f2678c7dc0f5eb0e91d0
potato dog man <saumil707@gmail.com>
2022-11-05 12:13:33 +0800

    buttons work now

--- grass/PlantSelectionView.swift
+++ grass/PlantSelectionView.swift
@@ -31,24 +31,25 @@ struct PlantSelectionView: View {
                     }
                 } label: {
                     Text("Select Plant")
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 87.75)
+                        .foregroundColor(.white)
+                        .background(Color("Swamp Green"))
+                        .cornerRadius(22.0)
                 }
-                .padding(.vertical, 20)
-                .padding(.horizontal, 87.75)
-                .foregroundColor(.white)
-                .background(Color("Swamp Green"))
-                .cornerRadius(22.0)
+
                 
                 
                 NavigationLink(destination: AddCustomPlantView()) {
                     Text("My plant isn't here!")
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 60.75)
+                        .foregroundColor(.white)
+                        .background(Color("Swamp Green"))
+                        .cornerRadius(22.0)
                     
                 }
-                
-                .padding(.vertical, 20)
-                .padding(.horizontal, 60.75)
-                .foregroundColor(.white)
-                .background(Color("Swamp Green"))
-                .cornerRadius(22.0)
+
             }
         }
     }
