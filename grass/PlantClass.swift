485d484268b6926570a40de60fa7db0b702392fe
attemptn63 <jeongwonho64@gmail.com>
2022-11-05 10:12:27 +0800

    Plant Struct added

---
+++ grass/PlantClass.swift
@@ -0,0 +1,11 @@
+import Foundation
+struct Plant{
+    var name: String
+    var scientificName: String
+    var wateringFrequency: Int
+    var wateringGuide: String
+    var fertilisationFrequency: Int
+    var fertilisationGuide: String
+    var temperatureRangeBegin: Int
+    var temperatureRangeEnd: Int
+}
