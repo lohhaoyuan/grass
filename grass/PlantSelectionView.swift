17de6e2bca2f28ddbb272f31c89f27dc4bbe2367
potato dog man <saumil707@gmail.com>
2022-11-12 20:45:12 +0800

    removed pop-up button

--- grass/PlantSelectionView.swift
+++ grass/PlantSelectionView.swift
@@ -17,32 +17,10 @@ struct PlantSelectionView: View {
                     .multilineTextAlignment(.leading)
                     .foregroundColor(Color("Swamp Green"))
                     .padding(.all, 18.0)
-                Menu {
-                    Button {
-                        //set some variable/initialise array
-                    } label: {
-                        Text("Plant")
-                    }
-                    Button {
-                        //set some variable/initialise array
-                        
-                    } label: {
-                        Text("Plant")
-                    }
-                } label: {
-                    Text("Select Plant")
-                        .padding(.vertical, 20)
-                        .padding(.horizontal, 87.75)
-                        .foregroundColor(.white)
-                        .background(Color("Swamp Green"))
-                        .cornerRadius(22.0)
-                        .animation(.easeIn, value: 5)
-                        .shadow(radius: 20)
-                    
-                }
-                .padding(9)
                 
+
                 
+                Spacer()
                 NavigationLink(destination: AddCustomPlantView()) {
                     Text("My plant isn't here!")
                         .padding(.vertical, 20)
