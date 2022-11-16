6164bc151034b97970fc9036eab9881735f0016d
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-11-16 14:29:15 +0800

    .

--- grass/ContentView.swift
+++ grass/ContentView.swift
@@ -32,50 +32,6 @@ struct ContentView: View {
     @StateObject var plantManager = PlantManager()
     
     var body: some View {
-        
-        VStack {
-            Button("Schedule Notification") {
-                
-                let center = UNUserNotificationCenter.current()
-                
-                // create content
-                let content = UNMutableNotificationContent()
-                content.title = "Hot Coffee"
-                content.body =  "Your delicious coffee is ready!"
-                content.categoryIdentifier = NotificationCategory.general.rawValue
-                content.userInfo = ["customData": "Some Data"]
-                
-                if let url = Bundle.main.url(forResource: "coffee", withExtension: "png") {
-                    if let attachment = try? UNNotificationAttachment(identifier: "image", url: url, options: nil) {
-                        content.attachments = [attachment]
-                    }
-                }
-                
-                // create trigger
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
-                
-                // create request
-                let request = UNNotificationRequest(identifier: "Identifier", content: content, trigger: trigger)
-                
-                // define actions
-                let dismiss = UNNotificationAction(identifier: NotificationAction.dimiss.rawValue, title: "Dismiss", options: [])
-                
-                let reminder = UNNotificationAction(identifier: NotificationAction.reminder.rawValue, title: "Reminder", options: [])
-                
-                let generalCategory = UNNotificationCategory(identifier: NotificationCategory.general.rawValue, actions: [dismiss, reminder], intentIdentifiers: [], options: [])
-                
-                center.setNotificationCategories([generalCategory])
-                
-                // add request to notification center
-                center.add(request) { error in
-                    if let error = error {
-                        print(error)
-                    }
-                }
-            
-            }
-        }
-        
         NavigationView {
             List {
                 ForEach($plantManager.plants) { $plant in
@@ -114,3 +70,48 @@ struct ContentView: View {
         
     }
 }
+
+/*
+ Saumils Notification Code
+ VStack {
+     Button("Schedule Notification") {
+         
+         let center = UNUserNotificationCenter.current()
+         
+         // create content
+         let content = UNMutableNotificationContent()
+         content.title = "Hot Coffee"
+         content.body =  "Your delicious coffee is ready!"
+         content.categoryIdentifier = NotificationCategory.general.rawValue
+         content.userInfo = ["customData": "Some Data"]
+         
+         if let url = Bundle.main.url(forResource: "coffee", withExtension: "png") {
+             if let attachment = try? UNNotificationAttachment(identifier: "image", url: url, options: nil) {
+                 content.attachments = [attachment]
+             }
+         }
+         
+         // create trigger
+         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+         
+         // create request
+         let request = UNNotificationRequest(identifier: "Identifier", content: content, trigger: trigger)
+         
+         // define actions
+         let dismiss = UNNotificationAction(identifier: NotificationAction.dimiss.rawValue, title: "Dismiss", options: [])
+         
+         let reminder = UNNotificationAction(identifier: NotificationAction.reminder.rawValue, title: "Reminder", options: [])
+         
+         let generalCategory = UNNotificationCategory(identifier: NotificationCategory.general.rawValue, actions: [dismiss, reminder], intentIdentifiers: [], options: [])
+         
+         center.setNotificationCategories([generalCategory])
+         
+         // add request to notification center
+         center.add(request) { error in
+             if let error = error {
+                 print(error)
+             }
+         }
+     }
+ }
+ */
