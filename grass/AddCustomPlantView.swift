7965aac0d0f2bc9d41d19884fe1761861cfa11e3
hao yuan <95842688+LHY-42@users.noreply.github.com>
2022-11-24 09:51:49 +0800

    tabviewstyle

--- grass/AddCustomPlantView.swift
+++ grass/AddCustomPlantView.swift
@@ -22,83 +22,87 @@ struct AddCustomPlantView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack{
-            Form {
-                Section(header: Text("Basic Details")) {
-                    TextField("Plant Name", text: $plantName)
-                    TextField("Scientific Name (Optional)", text: $scienceName)
-                }
-                Section(header: Text("Habitat")) {
-                    HStack {
-                        Text("Minimum Temperature")
-                        TextField("", value: $minTemp, formatter: NumberFormatter())
+        ZStack{
+            Color.gray
+                .ignoresSafeArea()
+            
+            VStack{
+                Form {
+                    Section(header: Text("Basic Details")) {
+                        TextField("Plant Name", text: $plantName)
+                        TextField("Scientific Name (Optional)", text: $scienceName)
+                    }
+                    Section(header: Text("Habitat")) {
+                        HStack {
+                            Text("Minimum Temperature")
+                            TextField("", value: $minTemp, formatter: NumberFormatter())
                                 .frame(width: 50)
                                 .padding(.leading)
-                        Text("˚C")
-                        Stepper(value: $minTemp, in: -20...40) {
+                            Text("˚C")
+                            Stepper(value: $minTemp, in: -20...40) {
                                 EmptyView()
+                            }
                         }
-                    }
-                    HStack {
-                        Text("Maximum Temperature")
-                        TextField("", value: $maxTemp, formatter: NumberFormatter())
+                        HStack {
+                            Text("Maximum Temperature")
+                            TextField("", value: $maxTemp, formatter: NumberFormatter())
                                 .frame(width: 50)
                                 .padding(.leading)
-                        Text("˚C")
-                        Stepper(value: $maxTemp, in: -19...41) {
+                            Text("˚C")
+                            Stepper(value: $maxTemp, in: -19...41) {
                                 EmptyView()
+                            }
                         }
                     }
-                }
-                Section(header: Text("Watering")) {
-                    HStack {
-                        Text("Frequency")
-                        TextField("", value: $waterFreq, formatter: NumberFormatter())
+                    Section(header: Text("Watering")) {
+                        HStack {
+                            Text("Frequency")
+                            TextField("", value: $waterFreq, formatter: NumberFormatter())
                                 .frame(width: 50)
                                 .padding(.leading)
-                        Text("days")
-                        Stepper(value: $waterFreq, in: 1...30) {
+                            Text("days")
+                            Stepper(value: $waterFreq, in: 1...30) {
                                 EmptyView()
+                            }
                         }
+                        TextField("Watering Instructions", text: $waterInstruct)
+                        
                     }
-                    TextField("Watering Instructions", text: $waterInstruct)
-                    
-                }
-                Section(header: Text("Fertilisation")) {
-                    HStack {
-                        Text("Frequency")
-                        TextField("", value: $fertFreq, formatter: NumberFormatter())
+                    Section(header: Text("Fertilisation")) {
+                        HStack {
+                            Text("Frequency")
+                            TextField("", value: $fertFreq, formatter: NumberFormatter())
                                 .frame(width: 50)
                                 .padding(.leading)
-                        Text("days")
-                        Stepper(value: $fertFreq, in: 1...8) {
+                            Text("days")
+                            Stepper(value: $fertFreq, in: 1...8) {
                                 EmptyView()
+                            }
                         }
+                        TextField("Fertilisation Instructions", text: $fertInstruct)
                     }
-                    TextField("Fertilisation Instructions", text: $fertInstruct)
-                }
-                Button("Save") {
-                    
-                    
-                    
-                    let plant = Plant(
-                        name: plantName,
-                        scientificName: scienceName,
-                        wateringFrequency: waterFreq,
-                        wateringGuide: waterInstruct,
-                        fertilisationFrequency: fertFreq,
-                        fertilisationGuide: fertInstruct,
-                        temperatureRangeBegin: minTemp,
-                        temperatureRangeEnd: maxTemp
-                    )
-                    plants.append(plant)
-                    dismiss()
+                    Button("Save") {
+                        
+                        
+                        
+                        let plant = Plant(
+                            name: plantName,
+                            scientificName: scienceName,
+                            wateringFrequency: waterFreq,
+                            wateringGuide: waterInstruct,
+                            fertilisationFrequency: fertFreq,
+                            fertilisationGuide: fertInstruct,
+                            temperatureRangeBegin: minTemp,
+                            temperatureRangeEnd: maxTemp
+                        )
+                        plants.append(plant)
+                        dismiss()
+                    }
                 }
             }
+            .navigationTitle("Add Custom Plant")
         }
-        .navigationTitle("Add Custom Plant")
-    }
-}
+    }}
 
 struct AddCustomPlantView_Previews: PreviewProvider {
     static var previews: some View {
