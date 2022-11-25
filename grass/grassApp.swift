8d2efc799fba5209de9e03d36bc0f67599fb7b6b
saumil <saumil707@gmail.com>
2022-11-25 09:31:35 +0800

    notification 2.5/3

--- grass/grassApp.swift
+++ grass/grassApp.swift
@@ -29,7 +29,7 @@ struct grassApp: App {
                 ContentView()
                     .preferredColorScheme(.light)
             } else {
-                PlantSelectionView()
+                OnboardingView()
             }
         }
     }
